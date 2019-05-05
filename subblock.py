94887ea51514beb4eb5510af5583a44409607607
Chris Lin <chris4540@gmail.com>
2019-05-05 23:20:07 +0200

    Add python script to show subblocking

---
+++ subblock.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Basic checking to ensure teacher subnet and the student subnet
+produce a equal sized intermediate img
+"""
+
+import torch
+import torch.nn as nn
+
+def get_sum_params(model):
+    ret = 0
+    for p in model.parameters():
+        ret += p.numel()
+    return ret
+
+if __name__ == "__main__":
+    # The first block of the teacher net
+    img = torch.randn(2,3,32,32)
+    teacher_subnet = [
+        nn.Conv2d(3, 64, kernel_size=3, padding=1),
+        nn.ReLU(inplace=True),
+        nn.Conv2d(64, 64, kernel_size=3, padding=1),
+        nn.ReLU(inplace=True),
+    ]
+    teacher_submodel = nn.Sequential(*teacher_subnet)
+    inter_img1 = teacher_submodel(torch.randn(2,3,32,32))
+    print("The size of the teacher subnet output: ", inter_img1.shape)
+
+    # The first block of the teacher net
+    # student
+    student_block = [
+        nn.Conv2d(3, 64//2, kernel_size=3, padding=1),
+        nn.ReLU(inplace=True),
+        nn.Conv2d(64//2, 64//2, kernel_size=3, padding=1),
+        nn.ReLU(inplace=True),
+        nn.Conv2d(64//2, 64, kernel_size=1, padding=0),
+    ]
+    student_submodel = nn.Sequential(*student_block)
+    inter_img2 = student_submodel(torch.randn(2,3,32,32))
+    print("The size of the student subnet output: ", inter_img2.shape)
+    assert inter_img1.shape == inter_img2.shape
+
+    # ===================================================================
+    print("# of params in teacher submodel:", get_sum_params(teacher_submodel))
+    print("# of params in student submodel:", get_sum_params(student_submodel))
+
