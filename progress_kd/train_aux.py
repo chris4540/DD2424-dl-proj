1783809f1c768d2270446918db8d64f79c5f1904
Chris Lin <chris4540@gmail.com>
2019-05-08 00:47:08 +0200

    Add train aux

--- progress_kd/train_aux.py
+++ progress_kd/train_aux.py
@@ -23,6 +23,7 @@ def get_optimizer(model):
 if __name__ == "__main__":
 
     batch_size = 100
+    epochs = 5
     # ==============================================
     teacher = Vgg('VGG16')
     chkpt = torch.load("vgg_16_teacher_chkpt.tar")
@@ -31,37 +32,33 @@ if __name__ == "__main__":
     if device == 'cuda':
         teacher.half()
 
-    # testloader = get_test_cifar10_dataloader('../../data', batch_size=100)
-
-    # score = evalation(testloader, teacher, device=device)
-    # print("Teacher score: ", score)
-
-    # train first aux
-    aux1 = AuxiliaryVgg(teacher, 1)
-    aux1.to(device)
-    if device == 'cuda':
-        aux1.half()
-
     # =========================================================================
     trainloader, validloader = get_train_valid_cifar10_dataloader('../../data', batch_size)
-    optimizer = get_optimizer(aux1)
-    scheduler = optim.lr_scheduler.CyclicLR(optimizer, 1e-5, 1e-2)
-    best_score = -np.inf
-    for epoch in range(0, 20):
-        print("Epoch:", epoch)
-        # train for one epoch
-        train(trainloader, aux1, optimizer, scheduler, device)
+    for phase_idx in range(1, 6):
+        print("phase: ", phase_idx)
+        print("Making aux {} network...", phase_idx)
+        student = AuxiliaryVgg(teacher, phase_idx)
+        student.to(device)
+        if device == 'cuda':
+            student.half()
+        optimizer = get_optimizer(student)
+        scheduler = optim.lr_scheduler.CyclicLR(optimizer, 1e-5, 1e-2)
+        best_score = -np.inf
+        for epoch in range(0, epochs):
+            print("Epoch:", epoch)
+            # train for one epoch
+            train(trainloader, student, optimizer, scheduler, device)
 
-        # evaluate on validation set
-        valid_score = evalation(validloader, aux1, device)
-        print("Validation Score: ", valid_score)
+            # evaluate on validation set
+            valid_score = evalation(validloader, student, device)
+            print("Validation Score: ", valid_score)
 
-        if valid_score > best_score:
-            best_score = max(valid_score, best_score)
-            saving_dict = {
-                'epoch': epoch+1,
-                'state_dict': aux1.state_dict(),
-                'validation_score': valid_score
-            }
-            torch.save(saving_dict, 'aux1_chkpt.tar')
+            if valid_score > best_score:
+                best_score = max(valid_score, best_score)
+                saving_dict = {
+                    'epoch': epoch+1,
+                    'state_dict': student.state_dict(),
+                    'validation_score': valid_score
+                }
+                torch.save(saving_dict, 'aux{}_chkpt.tar'.format(phase_idx))
 
