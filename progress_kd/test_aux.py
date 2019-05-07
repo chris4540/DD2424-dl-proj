02edd74d0cd61a1c90bd071cc242031ef9fc1370
Chris Lin <chris4540@gmail.com>
2019-05-08 00:15:07 +0200

    Add test code

--- progress_kd/test_aux.py
+++ progress_kd/test_aux.py
@@ -2,21 +2,27 @@ import torch
 from models.vgg import AuxiliaryVgg
 from models.vgg import Vgg
 
-def get_sum_params(model):
-    ret = 0
-    for p in model.parameters():
-        ret += p.numel()
-    return ret
-
 if __name__ == "__main__":
     teacher = Vgg('VGG16')
-    net = AuxiliaryVgg(teacher, 1)
-    net.drop_teacher_subnet_blk()
-    print("# of params = ", get_sum_params(net))
-    for k in range(2, 6):
-        student = AuxiliaryVgg(net, k)
-        student.drop_teacher_subnet_blk()
-        print("# of params = ", get_sum_params(student))
-        net = student
+    chkpt = torch.load("vgg_16_teacher_chkpt.tar")
+    teacher.load_state_dict(chkpt['state_dict'])
+    aux1 = AuxiliaryVgg(teacher, 1)
+    aux1.drop_teacher_subnet_blk()
+    data = {
+        'state_dict': aux1.state_dict()
+    }
+    torch.save(data, 'aux1_state.tar')
+
+    teacher_state = torch.load("vgg_16_teacher_chkpt.tar")['state_dict']
+    aux1_state = torch.load("aux1_state.tar")['state_dict']
+    assert torch.equal(teacher_state['features.7.weight'].float().to('cpu'), aux1_state['features.10.weight'].float())
+
+    # net.drop_teacher_subnet_blk()
+    # print("# of params = ", get_sum_params(net))
+    # for k in range(2, 6):
+    #     student = AuxiliaryVgg(net, k)
+    #     student.drop_teacher_subnet_blk()
+    #     print("# of params = ", get_sum_params(student))
+    #     net = student
 
 
