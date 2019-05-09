8288cc28c1e28c64394ae98cfdc68e89f3c74fd0
Chris Lin <chris4540@gmail.com>
2019-05-09 18:03:27 +0200

    Add fine tuning script for fully connected vgg16bn

--- progress_kd/fine_tune.py
+++ progress_kd/fine_tune.py
@@ -9,6 +9,7 @@ import torch.backends.cudnn as cudnn
 from utils.load_data import get_train_valid_cifar10_dataloader
 from utils import evalation
 from utils import train
+import numpy as np
 #
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 if device == 'cuda':
@@ -16,20 +17,17 @@ if device == 'cuda':
 #
 
 if __name__ == "__main__":
-    teacher = models.vgg16(pretrained=True)
+    teacher = models.vgg16_bn(pretrained=True)
     teacher.get_loss = nn.CrossEntropyLoss()
     num_classes = 10
-    # freeze all params
-    for p in teacher.parameters():
-        p.requires_grad = False
-    # change the classifier
+    chk_pt_file = "vgg16bn_teacher.tar"
 
     # teacher.classifier[3] = nn.Linear(num_ftrs, num_classes)
-    # num_ftrs = teacher.classifier[6].in_features
-    # last_layer = nn.Linear(num_ftrs, num_classes)
-    # torch.nn.init.kaiming_normal_(last_layer.weight.data)
-    # last_layer.bias.data.zero_()
-    # teacher.classifier[6] = last_layer
+    num_ftrs = teacher.classifier[6].in_features
+    last_layer = nn.Linear(num_ftrs, num_classes)
+    torch.nn.init.kaiming_normal_(last_layer.weight.data)
+    last_layer.bias.data.zero_()
+    teacher.classifier[6] = last_layer
 
     teacher.to(device)
     if device == 'cuda':
@@ -39,12 +37,12 @@ if __name__ == "__main__":
     l2_reg_weight = 5e-4
     optimizer = optim.SGD(teacher.parameters(),
         lr=0.05, momentum=0.9, weight_decay=l2_reg_weight)
-#     scheduler = optim.lr_scheduler.CyclicLR(optimizer, 1e-5, 1e-2)
-    scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.1)
+    scheduler = optim.lr_scheduler.CyclicLR(optimizer, 1e-5, 1e-2)
+    # scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.1)
     # ======================================================
     trainloader, validloader = get_train_valid_cifar10_dataloader('../../data', batch_size=100)
-
-    for epoch in range(0, 100):
+    best_score = -np.inf
+    for epoch in range(0, 10):
         print("Epoch:", epoch)
         # train for one epoch
         train(trainloader, teacher, optimizer, scheduler, device)
@@ -52,3 +50,11 @@ if __name__ == "__main__":
         # evaluate on validation set
         valid_score = evalation(validloader, teacher, device)
         print("Validation Score: ", valid_score)
+        if valid_score > best_score:
+            best_score = max(valid_score, best_score)
+            saving_dict = {
+                'epoch': epoch+1,
+                'state_dict': teacher.state_dict(),
+                'validation_score': valid_score
+            }
+            torch.save(saving_dict, chk_pt_file)
