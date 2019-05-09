cab47bfdfbc2280db3dd50b6f5649f1bfcce089a
Chris Lin <chris4540@gmail.com>
2019-05-09 20:02:15 +0200

    Add checking the test score

--- progress_kd/make_vgg16bn_teacher.py
+++ progress_kd/make_vgg16bn_teacher.py
@@ -7,6 +7,7 @@ import torchvision.models as models
 import torch.optim as optim
 import torch.backends.cudnn as cudnn
 from utils.load_data import get_train_valid_cifar10_dataloader
+from utils.load_data import get_test_cifar10_dataloader
 from utils import evalation
 from utils import train
 import numpy as np
@@ -20,6 +21,7 @@ if __name__ == "__main__":
     num_classes = 10
     nepochs = 20
     teacher_model_file = "vgg16bn_teacher.tar"
+    batch_size = 100
     # ===============================================================
     teacher = models.vgg16_bn(pretrained=True)
     teacher.get_loss = nn.CrossEntropyLoss()
@@ -41,7 +43,7 @@ if __name__ == "__main__":
     scheduler = optim.lr_scheduler.CyclicLR(optimizer, 1e-5, 1e-2)
     # scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.1)
     # ======================================================
-    trainloader, validloader = get_train_valid_cifar10_dataloader('../../data', batch_size=100)
+    trainloader, validloader = get_train_valid_cifar10_dataloader('../../data', batch_size)
     best_score = -np.inf
     for epoch in range(0, nepochs):
         print("Epoch:", epoch)
@@ -54,11 +56,21 @@ if __name__ == "__main__":
         if valid_score > best_score:
             # update the score
             best_score = valid_score
-
+            best_states = teacher.state_dict()
             saving_dict = {
                 'epoch': epoch+1,
-                'state_dict': teacher.state_dict(),
+                'state_dict': best_states,
                 'validation_score': valid_score
             }
             # save when the performance is better
             torch.save(saving_dict, teacher_model_file)
+    # ======================================================================
+    # Evaluation on test
+    # ======================================================================
+    # 1. update the teacher with the best model
+    teacher.load_state_dict(best_states)
+
+    # 2. test the best model
+    testloader = get_test_cifar10_dataloader('../../data', batch_size)
+    score = evalation(testloader, teacher, device)
+    print("The test accurancy/score is :", score)
