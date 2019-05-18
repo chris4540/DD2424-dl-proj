531749f5467bc31d2c8556a6e6b791d23918ecf1
Chris Lin <chris4540@gmail.com>
2019-05-19 00:24:19 +0200

    Update train student code

--- progress_kd/train_student.py
+++ progress_kd/train_student.py
@@ -1,96 +1,46 @@
-from models.vgg import VggStudent
-from models.vgg import Vgg
+"""
+Train a student from scatch
+"""
 import torch
-import torchvision
-import torch.backends.cudnn as cudnn
-import torch.optim as optim
 import torch.nn as nn
-import time
-import os
-import numpy as np
+import torch.optim as optim
+from models.vgg import VggStudent
+import torch.backends.cudnn as cudnn
+from utils import progressbar
 from utils import evalation
 from utils import train
-from utils import get_img_tranformation
 from utils.load_data import get_train_valid_cifar10_dataloader
 from utils.load_data import get_test_cifar10_dataloader
-import argparse
+import numpy as np
 
+
+#
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 if device == 'cuda':
     cudnn.benchmark = True
-
-def run_test(testloader, net, device):
-    print("Evaluating the model with the test set")
-    score = evalation(testloader, net, device)
-    print("Test score: ", score)
+#
 
 if __name__ == "__main__":
-    # Settings
-    checkpoint = None
-    # batch size
     batch_size = 100
-    # L2 regularization weight / L2 penalty
-    l2_reg_weight = 5e-4
-    lr = 0.05
-    # ========================================================================
-    # arg parse
-    is_resume = False
-    is_eval = False
-    is_batchnorm = True
-    role = "student"
-    epochs = 200
-    # =================================================================
-    if is_batchnorm:
-        model_name = 'vgg16bn'
-    else:
-        model_name = 'vgg16'
-    # ================================================
-    # ==========================================================================
-    # cpu or gpu
-    print("================================")
-    print("Going to use deive : ", device)
-    print("Model role: ", role)
-    print("Model name: ", model_name)
-    print("================================")
-    #
-    if checkpoint:
-        start_epoch = checkpoint['epoch']
-    else:
-        start_epoch = 0
-    # define the img trasformation
-    ###########################################################################
-    ## obtain the dataset
+    epochs = 50
+    net = VggStudent("VGG16", batch_norm=True)
+    net.to(device)
+    if device == 'cuda':
+        net.half()
 
+    # load teacher logits
+    logit_data = torch.load("teacher_logits_100.tar")
+    assert logit_data['batch_size'] == batch_size
+    logits = logit_data['logits']
+
+    # ===========================================================================
     trainloader, validloader = get_train_valid_cifar10_dataloader('../../data', batch_size)
     testloader = get_test_cifar10_dataloader('../../data', batch_size)
-    ###########################################################################
-    if role == "teacher":
-        net = Vgg("VGG16", batch_norm=is_batchnorm)
-    else:
-        net = VggStudent("VGG16", batch_norm=is_batchnorm)
-
-    optimizer = optim.SGD(net.parameters(),
-        lr=lr, momentum=0.9, weight_decay=l2_reg_weight)
-
-    if checkpoint:
-        net.load_state_dict(checkpoint['state_dict'])
-        optimizer.load_state_dict(checkpoint['optimizer'])
-
-    net = net.to(device)
-
-    if device == 'cuda':
-        net.half()  # use half precision
-
-    if is_eval:
-        run_test(testloader, net, device)
-        import sys
-        sys.exit(0)
-
-    # calculate step size
-    scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=30, gamma=0.1)
+    step_size = 2*np.int(np.floor(len(trainloader)/batch_size))
+    optimizer = optim.SGD(net.parameters(), lr=0.01, momentum=0.9, weight_decay=5e-4)
+    scheduler = optim.lr_scheduler.CyclicLR(optimizer, 1e-5, 1e-2, step_size_up=step_size)
     best_score = -np.inf
-
-    for epoch in range(start_epoch, epochs):
+    for epoch in range(0, epochs):
         print("Epoch:", epoch)
         # train for one epoch
         train(trainloader, net, optimizer, scheduler, device)
@@ -100,14 +50,18 @@ if __name__ == "__main__":
         print("Validation Score: ", valid_score)
 
         if valid_score > best_score:
-            best_score = max(valid_score, best_score)
+            best_score = valid_score
+            best_model_state_dict = net.state_dict()
             saving_dict = {
                 'epoch': epoch+1,
-                'state_dict': net.state_dict(),
-                'optimizer': optimizer.state_dict(),
-                'validation_score': valid_score,
+                'state_dict': best_model_state_dict,
+                'validation_score': valid_score
             }
-            chk_pt_file = './{}_{}_ct.tar.StepLR.{}'.format(model_name, role, epoch+1)
-            torch.save(saving_dict, chk_pt_file)
-    # ====================================================================
-    run_test(testloader, net, device)
+            torch.save(saving_dict, 'student.scratch.tar')
+
+    # test
+    # update teacher with the best parameters
+    net.load_state_dict(best_model_state_dict)
+    teacher = net
+    score = evalation(testloader, net, device)
+    print("The test accurancy/score is :", score)
