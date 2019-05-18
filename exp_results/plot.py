76edf66ae80a92f95e9f0a4220640567bee62cdd
Chris Lin <chris4540@gmail.com>
2019-05-19 00:59:45 +0200

    Update block acc

--- exp_results/plot.py
+++ exp_results/plot.py
@@ -1,6 +1,15 @@
 import pandas as pd
+from matplotlib.ticker import MaxNLocator
 
 if __name__ == "__main__":
+    df = pd.read_csv("block_acc.csv")
+    df = df.set_index('stage')
+    ax = df.plot(kind='line', style='x-')
+    ax.set_ylabel("Accuracy")
+    ax.xaxis.set_major_locator(MaxNLocator(integer=True))
+    fig = ax.get_figure()
+    fig.savefig("pbt_stage_acc.png", bbox_inches='tight')
+    # ======================================================
     df = pd.read_csv("progressive_block_exp.csv")
     df = df[df['epoch'] == 10]
     df = df[df['alpha'] < 2]
