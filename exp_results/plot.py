372441af1eab8499f350a6c31ce125207a449870
Chris Lin <chris4540@gmail.com>
2019-05-19 21:41:57 +0200

    Update plot

--- exp_results/plot.py
+++ exp_results/plot.py
@@ -12,13 +12,13 @@ if __name__ == "__main__":
     # ======================================================
     df = pd.read_csv("progressive_block_exp.csv")
     df = df[df['epoch'] == 10]
-    df = df[df['alpha'] < 2]
     cols = ['alpha', 'test', 'validation']
     df2 = df[cols]
     df2 = df2.set_index('alpha')
     df2 = df2.sort_index()
 
     ax = df2.plot(kind='line', style='x-')
+    ax.set_xscale('log')
     ax.set_ylabel("Accuracy")
     # ax.set_ylim([0.75, 0.9])
     fig = ax.get_figure()
